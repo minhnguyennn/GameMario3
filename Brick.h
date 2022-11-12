4d1e9dfb7e82d6b2938d078b4d4f3c6830018c5c
minhnguyennn <minh842657913@gmail.com>
2022-11-12 12:39:53 +0700

    Create animation for yellow brick

--- Brick.h
+++ Brick.h
@@ -5,7 +5,13 @@
 #include "QuestionBrick.h"
 #include "Animations.h"
 
+//ANIMATION
 #define ID_ANI_BRICK 10000
+#define ID_ANI_BRICK_NULL 10001
+#define ID_ANI_BUTTON_NOT_PRESSED 10002
+#define ID_ANI_BUTTON_PRESSED 10003
+
+//BBOX
 #define BRICK_WIDTH 16
 #define BRICK_BBOX_WIDTH 16
 #define BRICK_BBOX_HEIGHT 16
